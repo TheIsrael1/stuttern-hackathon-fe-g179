654462ba0a232a84272f93dc0f184fff3a19a288
TheIsrael1 <ehindero2022@gmail.com>
2023-11-27 21:33:46 +0100

    chore -debug cors issue

--- adapters/auth/index.tsx
+++ adapters/auth/index.tsx
@@ -2,12 +2,16 @@ import { headers } from 'next/headers';
 import { myFetch } from '..';
 
 const getToken = async () => {
-  const res = await myFetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/token`, {
-    headers: headers()
-  });
-  const data = res.json();
+  try {
+    const res = await myFetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/token`, {
+      headers: headers()
+    });
+    const data = res.json();
 
-  return data;
+    return data;
+  } catch (err) {
+    console.log(`Something happened!`, err);
+  }
 };
 
 const authService = { getToken };
