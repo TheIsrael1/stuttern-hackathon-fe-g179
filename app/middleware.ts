755f0eb2c5923570f787b13e668a77292b6c471e
TheIsrael1 <ehindero2022@gmail.com>
2023-11-27 22:04:42 +0100

    chore - redirect on error

--- app/middleware.ts
+++ app/middleware.ts
@@ -11,21 +11,6 @@ export async function middleware(request: NextRequest) {
     secret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET
   });
 
-  const secret = new TextEncoder().encode(process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET!);
-
-  const alg = 'HS256';
-
-  const jwt = await new jose.SignJWT({
-    name: token?.name,
-    email: token?.email
-  })
-    .setProtectedHeader({ alg })
-    .sign(secret);
-
-  requestHeaders.set('test', `test`);
-
-  console.log('reqqhhsss', requestHeaders);
-
   const response = NextResponse.next({
     request: {
       headers: requestHeaders
