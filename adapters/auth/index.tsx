621ed1e6f59e39c3b11a88e70e6bbaa907a866fe
TheIsrael1 <ehindero2022@gmail.com>
2023-11-27 13:50:54 +0100

    chore - added app base url as an environment var

--- adapters/auth/index.tsx
+++ adapters/auth/index.tsx
@@ -1,6 +1,5 @@
 import { headers } from 'next/headers';
 import { myFetch } from '..';
-import { processError } from '@/lib/error';
 
 const getToken = async () => {
   const res = await myFetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/token`, {
