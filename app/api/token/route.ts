9fc9a3afccc591245b050bb089baf88df4ee43cb
TheIsrael1 <ehindero2022@gmail.com>
2023-11-27 21:23:42 +0100

    chore -debug cors issue

--- app/api/token/route.ts
+++ app/api/token/route.ts
@@ -1,11 +1,11 @@
+import { NextApiResponse } from 'next';
 import { getToken } from 'next-auth/jwt';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
   const token = await getToken({
     req: request,
     secret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET
   });
-
-  return NextResponse.json({ token });
+  return Response.json({ token });
 }
